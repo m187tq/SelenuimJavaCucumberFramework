3a060479daa969f86851efa41d25fd4a26862415
Ben Azeta <automationframework2022@gmail.com>
2023-04-24 13:56:28 +0100

    First commit

--- src/test/java/com/cucumberOptions/TestNGTestRunner.java
+++ src/test/java/com/cucumberOptions/TestNGTestRunner.java
@@ -2,35 +2,25 @@ package com.cucumberOptions;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.DataProvider;
 
 @CucumberOptions(
         features = {"src/test/java/com/features/"},
         glue = {"com/stepDefinitions"},
 		monochrome = true,
-		//tags = "@LoginValid",
-		//tags = "@LoginValid_Positive",
-		//tags = "@NegativeLogin",
-		//tags = "@LoginValid",
         //tags = "@RegistrationFunctionality",
-		tags = "@e2e",
-		//tags = "@addMultipleItemsToCart",
-		//tags = "@register",
-		//tags = "@addRemoveItemsToCart",
-        //tags = "@NegativeLogin",
-        //dryRun = false,
-        //Strict = false,
-        //name = "Login",
+		tags = "@register",
         plugin = {"pretty",
                 "html:target/cucumber",
                 "json:target/cucumber.json",
                 "html:target/cucumber-reports/cucumber-pretty",
-				// "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
+				//"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
 				"rerun:target/failed_scenarios.txt"})
 
 public class TestNGTestRunner extends AbstractTestNGCucumberTests {
-   /* @Override
+   @Override
 	  @DataProvider(parallel=true)
 	public Object[][] scenarios() {
 		  return super.scenarios();
-	  }*/
+	  }
 }
